e1541da4e006aedd905528ee8fb4cf0cf115beec
Gaurav <gauravbanwal1234@gmail.com>
2026-01-16 23:22:53 +0530

    set the material theme

--- app/src/main/java/com/gaurav/smartcook/MainActivity.kt
+++ app/src/main/java/com/gaurav/smartcook/MainActivity.kt
@@ -6,19 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.gaurav.smartcook.ui.theme.SmartCookTheme
+import com.gaurav.smartcook.ui.theme.AppTheme
+import com.gaurav.smartcook.ui.theme.Nunito
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SmartCookTheme {
+            AppTheme() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -34,6 +39,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        style = MaterialTheme.typography.titleMedium,
         modifier = modifier
     )
 }
@@ -41,7 +47,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SmartCookTheme {
+    AppTheme() {
         Greeting("Android")
     }
 }
